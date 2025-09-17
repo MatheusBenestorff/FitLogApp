50f975a61e415205310a5e24b971cf1f6d10b2f5
Matheus Benestorff <matheusbenestorff@gmail.com>
2025-09-17 00:21:02 -0300

    Feat: Endpoint GetUserWorkoutById

--- fitlog-api/Data/Models/CreateWorkoutDto.cs
+++ fitlog-api/Data/Models/CreateWorkoutDto.cs
@@ -2,16 +2,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace FitLogApp.api.Data;
 
-public class Workout
+public class CreateWorkoutDto
 {
-    public int Id { get; set; }
-
     [Required]
     public string Name { get; set; }
 
-    public int UserId { get; set; }
-
-    public User User { get; set; }
-
-    public List<Exercise> Exercises { get; set; }
+    public List<int> ExerciseIds { get; set; }
 }
