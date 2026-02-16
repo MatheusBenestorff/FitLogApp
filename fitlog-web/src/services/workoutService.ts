9eadac347b2fcb6c7f1b3bd38eda75a95fa583ac
MatheusBenestorff <matheusbenestorff@gmail.com>
2026-02-16 18:53:21 -0300

    Add WorkoutService in frontend

---
+++ fitlog-web/src/services/workoutService.ts
@@ -0,0 +1,28 @@
+import api from "./api";
+import type { Workout, CreateWorkoutDto, UpdateWorkoutDto } from "../types/workout";
+
+export const workoutService = {
+  getAll: async () => {
+    const response = await api.get<Workout[]>("/Workout");
+    return response.data;
+  },
+
+  getById: async (id: number) => {
+    const response = await api.get<Workout>(`/Workout/${id}`);
+    return response.data;
+  },
+
+  create: async (data: CreateWorkoutDto) => {
+    const response = await api.post<Workout>("/Workout", data);
+    return response.data;
+  },
+
+  update: async (id: number, data: UpdateWorkoutDto) => {
+    const response = await api.put<Workout>(`/Workout/${id}`, data);
+    return response.data;
+  },
+
+  delete: async (id: number) => {
+    await api.delete(`/Workout/${id}`);
+  }
+};
